7d5ddd49965215a31934fef5abe12715b58ee1a4
RhexNiebres <rhexniebres@gmail.com>
2025-01-11 18:21:09 -0800

    add knightMoves function to shhhow shortest posible way to get from one square to another

--- src/index.js
+++ src/index.js
@@ -1,2 +1,24 @@
-import './styles.css'; // Import the CSS file
-console.log('Webpack setup complete!');
+function knightMoves(){
+
+    const possibleMoves = [
+        [2, 1], [2, -1], [-2, 1], [-2, -1],
+        [1, 2], [1, -2], [-1, 2], [-1, -2]
+    ];
+    
+    if (start[0] === end[0] && start[1] === end[1]) {
+        return `You made it in 0 moves! Here's your path:\n[${start}]`;
+    }
+
+    function isOnBoard(x, y) {// to check if the knight position is valid
+        return x >= 0 && x < 8 && y >= 0 && y < 8;
+    }
+
+    const queue = [[start]];
+    const visited = new Set();
+    visited.add(start.toString());
+
+    while (queue.length > 0) {
+        const currentPath = queue.shift();
+        const currentPosition = currentPath[currentPath.length - 1];
+    }
+}
